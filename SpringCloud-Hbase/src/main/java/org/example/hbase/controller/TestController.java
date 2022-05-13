ca70a40a7f5bc162723fcebbab8e52e17d5e4b14
jiabotao <724270650@qq.com>
2022-05-14 04:10:33 +0800

    spring-hbase

--- SpringCloud-Hbase/src/main/java/org/example/hbase/controller/TestController.java
+++ SpringCloud-Hbase/src/main/java/org/example/hbase/controller/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("test")
@@ -15,11 +16,11 @@ public class TestController {
     HbaseTemplate hbaseTemplate;
 
     @GetMapping("/extistTable")
-    public Boolean extistTable() {
+    public Boolean extistTable() throws IOException {
         //hbaseTemplate.createTable("jiabotao","a","b");
         //hbaseTemplate.deleteTable("jiabotao");
       //  hbaseTemplate.addColumnFamily("jiabotao","a","b");
-        hbaseTemplate.deleteColumnFamily("jiabotao","c","d","e");
+        hbaseTemplate.scan("jiabotao");
        // hbaseTemplate.addColumnFamily("jiabotao","c","d");
         return false;
     }
--- SpringCloud-Hbase/src/main/java/org/example/hbase/service/HbaseTemplate.java
+++ SpringCloud-Hbase/src/main/java/org/example/hbase/service/HbaseTemplate.java
@@ -1,16 +1,38 @@
 package org.example.hbase.service;
 
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.*;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.io.IOException;
+import java.util.Iterator;
 
 @Service
 public class HbaseTemplate extends HbaseTableTemplate{
 
 
-
+    public void scan(String tableName) throws IOException {
+        Connection conn = admin.getConnection();
+        Table table = conn.getTable(TableName.valueOf(tableName));
+        Scan scan = new Scan();
+        ResultScanner scanner = table.getScanner(scan);
+        Iterator<Result> resultIterator = scanner.iterator();
+        while (resultIterator.hasNext()){
+            Result result = resultIterator.next();
+            Cell[] cells = result.rawCells();
+            for(Cell cell:cells){
+                System.out.println("INFO:   "+cell);
+                System.out.println(new String(CellUtil.cloneRow(cell)));    //  行键
+                System.out.println(new String(CellUtil.cloneFamily(cell)));     //  列簇
+                System.out.println(new String(CellUtil.cloneQualifier(cell)));      //  字段
+                System.out.println(new String(CellUtil.cloneValue(cell)));      //  值
+                System.out.println(cell.getTimestamp());    //  时间戳
+            }
+        }
+        table.close();
+    }
 
 }
